9d8644f440fd82ab4874dcc242e9e4eb8c43f632
EngMarianaBrito <marianabrito1791@gmail.com>
2021-10-01 23:38:52 -0300

    Terminando primeira parte do projeto

---
+++ app_landing_page/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .models import *
+
+# Create your views here.
+
+def home(request):
+    dados = DadosSite.objects.all()
+    produtos = Produto.objects.all()
+    pessoas = Pessoa.objects.all()
+
+    dicionario = {
+        "dados_site": dados,
+        "lista_produtos" : produtos,
+        "lista_pessoas" : pessoas
+    }
+
+    return render(request, "index.html", dicionario)
+
+def login(request):
+
+    return render(request, "login.html")
